5341c936aa1a1e34579ff0f7fd87282415b77baf
phamcu <ccuong.ph@gmail.com>
2021-03-02 14:45:36 +0000

    update(vote): CRUD vote

--- modules/web_app/backend/src/controllers/auth.js
+++ modules/web_app/backend/src/controllers/auth.js
@@ -1,18 +1,19 @@
-import { CODE, ID } from "../constants/index";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import { secretKey, externalAuthUrl, hostName } from "../configs/index";
-import { v4 as uuidv4 } from "uuid";
-import axios from "axios";
-import transporter from "./utils/nodemailer";
-const Pool = require("pg").Pool;
+import axios from "axios"
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+import { v4 as uuidv4 } from "uuid"
+import { externalAuthUrl, hostName, secretKey } from "../configs/index"
+import { CODE } from "../constants/index"
+import transporter from "./utils/nodemailer"
+const Pool = require("pg").Pool
 const pool = new Pool({
   user: "admin",
   host: "3.1.100.54",
   database: "topdup_db",
   password: "uyL7WgydqKNkNMWe",
   port: "5432"
-});
+})
+
 const generatorToken = (userId, baseToken) => {
   return jwt.sign(
     {
@@ -20,58 +21,58 @@ const generatorToken = (userId, baseToken) => {
       baseToken
     },
     secretKey
-  );
-};
+  )
+}
 
 const confirmEmail = async (req, res) => {
   try {
-    const { userId, secret_code } = req.params;
+    const { userId, secret_code } = req.params
     const queryIsExist = `
     SELECT * 
     FROM public."user" 
     WHERE id = '${userId}' AND secret_code = '${secret_code}'
-    `;
-    let isExist = await pool.query(queryIsExist);
+    `
+    let isExist = await pool.query(queryIsExist)
     if (isExist.rows.length != 0) {
       const queryUpdate = `
         UPDATE public."user" 
         SET is_verified = '${true}'
         WHERE id = '${userId}'
-      `;
-      await pool.query(queryUpdate);
+      `
+      await pool.query(queryUpdate)
       res.json({
         code: CODE.SUCCESS,
         message: "Xác thực thành công!"
-      });
+      })
     }
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
 const register = async (req, res) => {
   try {
-    const { email, password, firstName, lastName } = req.body;
+    const { email, password, firstName, lastName } = req.body
     let queryIsExist = `
         SELECT *
         FROM public."user"
         WHERE email =  '${email}'
-        `;
-    let isExist = await pool.query(queryIsExist);
+        `
+    let isExist = await pool.query(queryIsExist)
     if (isExist.rows.length != 0) {
       res.json({
         code: CODE.OBJECT_EXIST,
         message: "Tài khoản đã tồn tại!"
-      });
+      })
     } else {
-      const hashPassword = bcrypt.hashSync(password, 8);
-      const secretCode = Math.ceil(Math.random() * 10000);
+      const hashPassword = bcrypt.hashSync(password, 8)
+      const secretCode = Math.ceil(Math.random() * 10000)
       const queryAddNewUser = `
             INSERT INTO public."user" (firstName, lastName, email, password,login, thumbnail, is_verified , secret_code)
             VALUES ('${firstName}',' ${lastName}','${email}', '${hashPassword}', 'false', 'xxx.yyy', 'false' , '${secretCode}')
             RETURNING *
-          `;
-      const result = await pool.query(queryAddNewUser);
+          `
+      const result = await pool.query(queryAddNewUser)
       var mailOptions = {
         from: "xxx@gmail.com",
         to: result.rows[0].email,
@@ -80,8 +81,8 @@ const register = async (req, res) => {
         html: `<p>Please use the following link within the next 10 minutes to activate your account on xxx APP: 
                 ${hostName}/api/v1/auth/verification/verify-account/${result.rows[0].id}/${secretCode}
                 <strong><a href="${hostName}/api/v1/auth/verification/verify-account/${result.rows[0].id}/${secretCode}" target="_blank">Email Topdup.xyz</a></strong></p>`
-      };
-      await transporter.sendMail(mailOptions);
+      }
+      await transporter.sendMail(mailOptions)
       if (result) {
         res.json({
           code: CODE.SUCCESS,
@@ -90,32 +91,32 @@ const register = async (req, res) => {
             name: `${result.rows[0].firstname} ${result.rows[0].lastname}`,
             verify: result.rows[0].is_verified
           }
-        });
+        })
       } else {
         res.json({
           code: CODE.ERROR,
           message: "Đăng ký thất bại!"
-        });
+        })
       }
     }
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
 const loginNormal = async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const { email, password } = req.body
     const query = `
       SELECT * 
       FROM public."user" 
       WHERE email = '${email}'
-    `;
-    let result = await pool.query(query);
+    `
+    let result = await pool.query(query)
     if (result.rows.length != 0) {
-      const compare = bcrypt.compareSync(password, result.rows[0].password);
+      const compare = bcrypt.compareSync(password, result.rows[0].password)
       if (compare) {
-        const accessToken = generatorToken(result.rows[0].id, uuidv4());
+        const accessToken = generatorToken(result.rows[0].id, uuidv4())
         res.json({
           code: CODE.SUCCESS,
           data: {
@@ -128,59 +129,59 @@ const loginNormal = async (req, res) => {
             accessToken: accessToken
           },
           message: "Đăng nhập thành công!"
-        });
+        })
       } else {
         res.json({
           code: CODE.INVALID,
           message: "Mật khẩu không đúng!"
-        });
+        })
       }
     } else {
       res.json({
         code: CODE.INVALID,
         message: "Tài khoản không đúng!"
-      });
+      })
     }
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
 const logout = async (req, res) => {
   try {
     res.json({
       code: CODE.SUCCESS,
       message: "Đăng xuất thành công!"
-    });
+    })
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
 const loginByFaceBook = async (req, res) => {
   try {
-    const { fbToken, fbId } = req.body;
-    const param = `me?fields=id,last_name,first_name,name,email,picture{height,cache_key,is_silhouette,url,width},gender`;
+    const { fbToken, fbId } = req.body
+    const param = `me?fields=id,last_name,first_name,name,email,picture{height,cache_key,is_silhouette,url,width},gender`
     const response = await axios.get(
       `${externalAuthUrl.fb}/${param}&access_token=${fbToken}`
-    );
-    const fbInfo = response.data;
+    )
+    const fbInfo = response.data
     if (response.status === 200 && fbInfo.id === fbId) {
       let queryIsExist = `
              SELECT *
                 FROM public."user"
                 WHERE email = '${fbInfo.email}'
-                `;
-      let isExist = await pool.query(queryIsExist);
+                `
+      let isExist = await pool.query(queryIsExist)
       if (isExist.rows.length != 0) {
         const queryUpdate = `
                 UPDATE public."user" 
                 SET email = '${fbInfo.email}', thumbnail = '${fbInfo.picture.data.url}'
                 WHERE id = '${isExist.rows[0].id}'
                 RETURNING *
-                `;
-        const result = await pool.query(queryUpdate);
-        const accessToken = generatorToken(result.rows[0].id, uuidv4());
+                `
+        const result = await pool.query(queryUpdate)
+        const accessToken = generatorToken(result.rows[0].id, uuidv4())
         res.json({
           code: CODE.SUCCESS,
           data: {
@@ -193,16 +194,16 @@ const loginByFaceBook = async (req, res) => {
             accessToken: accessToken
           },
           message: "Đăng nhập thành công!"
-        });
+        })
       } else {
         const queryNewUser = `
             INSERT INTO public."user" (firstName, lastName, thumbnail, email,login,  is_verified)
             VALUES ('${fbInfo.first_name}','${fbInfo.last_name}', '${fbInfo.picture.data.url}','${fbInfo.email}','true','true')
             RETURNING *
-            `;
-        const result = await pool.query(queryNewUser);
+            `
+        const result = await pool.query(queryNewUser)
         if (result) {
-          const accessToken = generatorToken(result.id, uuidv4());
+          const accessToken = generatorToken(result.id, uuidv4())
           res.json({
             code: CODE.SUCCESS,
             data: {
@@ -214,44 +215,44 @@ const loginByFaceBook = async (req, res) => {
               accessToken: accessToken
             },
             message: "Đăng nhập thành công!"
-          });
+          })
         } else {
           res.json({
             code: CODE.ERROR,
             message: "Đăng nhập thất bại!"
-          });
+          })
         }
       }
     }
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
 const loginByGoogle = async (req, res) => {
   try {
-    const { ggToken, ggId } = req.body;
-    const param = `userinfo?access_token=`;
+    const { ggToken, ggId } = req.body
+    const param = `userinfo?access_token=`
     const response = await axios.get(
       `${externalAuthUrl.gg}/${param}${ggToken}`
-    );
-    const ggInfo = response.data;
+    )
+    const ggInfo = response.data
     if (response.status === 200 && ggInfo.sub === ggId) {
       let queryIsExist = `
             SELECT *
                FROM public."user"
                WHERE email = ${ggInfo.email}
-               `;
-      let isExist = await pool.query(queryIsExist);
+               `
+      let isExist = await pool.query(queryIsExist)
       if (isExist.rows.length != 0) {
         const queryUpdate = `
                 UPDATE public."user" 
                 SET  email = '${ggInfo.email}', thumnail = '${ggInfo.picture}'
                 WHERE id = '${isExist.id}'
                 RETURNING *
-              `;
-        const result = await pool.query(queryUpdate);
-        const accessToken = generatorToken(result.rows[0].id, uuidv4());
+              `
+        const result = await pool.query(queryUpdate)
+        const accessToken = generatorToken(result.rows[0].id, uuidv4())
         res.json({
           code: CODE.SUCCESS,
           data: {
@@ -264,16 +265,16 @@ const loginByGoogle = async (req, res) => {
             accessToken: accessToken
           },
           message: "Đăng nhập thành công!"
-        });
+        })
       } else {
         const queryNewUser = `
                 INSERT INTO public."user" (firstName, lastName, thumbnail, email,login ,is_verified)
                 VALUES ( '${ggInfo.family_name}','${ggInfo.given_name}', '${ggInfo.name}', '${ggInfo.picture}','${ggInfo.email}','true','${ggInfo.verified_email}')
                 RETURNING *
-                `;
-        const result = await pool.query(queryNewUser);
+                `
+        const result = await pool.query(queryNewUser)
         if (result) {
-          const accessToken = generatorToken(result.id, uuidv4());
+          const accessToken = generatorToken(result.id, uuidv4())
           res.json({
             code: CODE.SUCCESS,
             data: {
@@ -286,68 +287,68 @@ const loginByGoogle = async (req, res) => {
               accessToken: accessToken
             },
             message: "Đăng nhập thành công!"
-          });
+          })
         } else {
           res.json({
             code: CODE.ERROR,
             message: "Đăng nhập thất bại!"
-          });
+          })
         }
       }
     }
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 const restPassword = async (req, res) => {
   try {
-    const { email, password, secret_code } = req.body;
-    const hashPassword = bcrypt.hashSync(password, 8);
+    const { email, password, secret_code } = req.body
+    const hashPassword = bcrypt.hashSync(password, 8)
     const queryUpdate = `
             UPDATE public."user" 
             SET password = '${hashPassword}'
             WHERE email = '${email}' AND secret_code = '${secret_code}'
             RETURNING *
-            `;
-    const result = await pool.query(queryUpdate);
+            `
+    const result = await pool.query(queryUpdate)
     if (result.rows.length != 0) {
       res.json({
         code: CODE.SUCCESS,
         message: "Đổi mật khẩu thành công!"
-      });
+      })
     } else {
       res.json({
         code: CODE.ERROR,
         message: "Đổi mật khẩu không thành công!"
-      });
+      })
     }
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 const genSecretCode = async (req, res) => {
   try {
-    const { email } = req.query;
+    const { email } = req.query
     let queryIsExist = `
             SELECT *
             FROM public."user"
             WHERE email =  '${email}'
             RETURNING *
-        `;
-    const result = await pool.query(queryIsExist);
+        `
+    const result = await pool.query(queryIsExist)
     var mailOptions = {
       from: "xxx@gmail.com",
       to: result.rows[0].email,
       subject: "Mã số bí mật",
       text: `Please use the following link within the next 10 minutes to activate your account on xxx APP: ${result.rows[0].secret_code}`
-    };
-    await transporter.sendMail(mailOptions);
+    }
+    await transporter.sendMail(mailOptions)
     res.json({
       code: CODE.SUCCESS,
       message: "Gửi email thành công!"
-    });
-  } catch (error) {}
-};
+    })
+  } catch (error) { }
+}
 
 export default {
   confirmEmail,
@@ -358,4 +359,4 @@ export default {
   loginByGoogle,
   restPassword,
   genSecretCode
-};
+}
--- modules/web_app/backend/src/controllers/similarity-report.js
+++ modules/web_app/backend/src/controllers/similarity-report.js
@@ -1,4 +1,5 @@
-const Pool = require("pg").Pool;
+
+const Pool = require("pg").Pool
 
 const pool = new Pool({
   user: "admin",
@@ -6,17 +7,84 @@ const pool = new Pool({
   database: "topdup_db",
   password: "uyL7WgydqKNkNMWe",
   port: "5432"
-});
+})
+
+const getSimRecordById = async (id, userId) => {
+  try {
+    const simReportsQuery = `
+      SELECT 
+        SR.id as id,
+        A1.title as article_a,
+        A1.id as article_a_id, 
+        A1.domain as domain_a,
+        A1.author as author_a, 
+        A1.created_date as created_date_a,
+        A2.title as article_b,
+        A2.id as article_b_id, 
+        A2.domain as domain_b,
+        A2.author as author_b, 
+        A2.created_date as created_date_b,
+        SR.sim_score as sim_score	
+      FROM public.similarity_report SR
+      LEFT JOIN ARTICLE A1 ON A1.id = SR.article_a_id
+      LEFT JOIN ARTICLE A2 ON A2.id = SR.article_b_id
+      WHERE SR.id = ${id}
+    `
+    const simReportRes = await pool.query(simReportsQuery)
+    const foundSimReport = simReportRes && simReportRes.rows && simReportRes.rows[0]
+
+    console.log('foundSimReport', foundSimReport)
+
+    if (!foundSimReport) return
+
+    const articleAId = foundSimReport['article_a_id']
+    const articleBId = foundSimReport['article_b_id']
+    const voteRes = await pool.query(`SELECT * FROM public.vote WHERE article_a_id = ${articleAId} AND article_b_id = ${articleBId}`)
+    const voteRecords = (voteRes && voteRes.rows) || []
+    const articleAVotes = voteRecords.filter(vote => vote['voted_option'] === 1)
+    const articleBVotes = voteRecords.filter(vote => vote['voted_option'] === 2)
+    const errorVotes = voteRecords.filter(vote => vote['voted_option'] === 3)
+    const irrelevantVotes = voteRecords.filter(vote => vote['voted_option'] === 4)
+    const foundVote = voteRecords.find(vote => vote.userId === userId)
+
+    return {
+      id: foundSimReport["id"],
+      articleA: foundSimReport["article_a"],
+      articleAId: foundSimReport["article_a_id"],
+      domainA: foundSimReport["domain_a"],
+      authorA: foundSimReport["author_a"],
+      createdDateA: foundSimReport["created_date_a"],
+      articleB: foundSimReport["article_b"],
+      articleBId: foundSimReport["article_b_id"],
+      domainB: foundSimReport["domain_b"],
+      authorB: foundSimReport["author_b"],
+      createdDateB: foundSimReport["created_date_b"],
+      articleANbVotes: articleAVotes.length,
+      articleBNbVotes: articleBVotes.length,
+      errorNbVotes: errorVotes.length,
+      irrelevantNbVotes: irrelevantVotes.length,
+      simScore: foundSimReport["sim_score"],
+      userVoted: foundVote !== undefined,
+      voteId: foundVote && foundVote.id,
+      votedOption: foundVote && foundVote['voted_option']
+    }
+  } catch (error) {
+    throw (error)
+  }
+}
 
 const getSimilarityRecords = async (request, response) => {
+  const userId = request.query.userId
   const getSimReportsQuery = `
     SELECT 
       SR.id as id,
-      A1.title as article_a, 
+      A1.title as article_a,
+      A1.id as article_a_id, 
       A1.domain as domain_a,
       A1.author as author_a, 
       A1.created_date as created_date_a,
-      A2.title as article_b, 
+      A2.title as article_b,
+      A2.id as article_b_id, 
       A2.domain as domain_b,
       A2.author as author_b, 
       A2.created_date as created_date_b,
@@ -24,31 +92,32 @@ const getSimilarityRecords = async (request, response) => {
     FROM public.similarity_report SR
     LEFT JOIN ARTICLE A1 ON A1.id = SR.article_a_id
     LEFT JOIN ARTICLE A2 ON A2.id = SR.article_b_id
-  `;
+  `
 
   const getVotesQuery = `SELECT * FROM public.vote`
 
   const getSimReportsRes = await pool.query(getSimReportsQuery)
-  const getVotesRes = await pool.query(getVotesQuery);
-  const simReports = getSimReportsRes.rows;
-  const votes = getVotesRes.rows;
+  const getVotesRes = await pool.query(getVotesQuery)
+  const simReports = getSimReportsRes.rows
+  const votes = getVotesRes.rows
 
   const results = simReports.map(report => {
-    const userId = 25
-    const voteRecords = votes.filter(vote => vote['article_a_id'] === report['article_a_id'] && vote['article_b_id'] === report['article_b_id']) 
+    const voteRecords = votes.filter(vote => vote['article_a_id'] === report['article_a_id'] && vote['article_b_id'] === report['article_b_id'])
     const articleAVotes = voteRecords.filter(vote => vote['voted_option'] === 1)
     const articleBVotes = voteRecords.filter(vote => vote['voted_option'] === 2)
     const errorVotes = voteRecords.filter(vote => vote['voted_option'] === 3)
     const irrelevantVotes = voteRecords.filter(vote => vote['voted_option'] === 4)
-    const foundVote = voteRecords.find(vote => vote.userId === userId)
-    
+    const foundVote = voteRecords.find(vote => vote['user_id'] === userId)
+
     return {
       id: report["id"],
       articleA: report["article_a"],
+      articleAId: report["article_a_id"],
       domainA: report["domain_a"],
       authorA: report["author_a"],
       createdDateA: report["created_date_a"],
       articleB: report["article_b"],
+      articleBId: report["article_b_id"],
       domainB: report["domain_b"],
       authorB: report["author_b"],
       createdDateB: report["created_date_b"],
@@ -58,12 +127,40 @@ const getSimilarityRecords = async (request, response) => {
       irrelevantNbVotes: irrelevantVotes.length,
       simScore: report["sim_score"],
       userVoted: foundVote !== undefined,
+      voteId: foundVote && foundVote.id,
       votedOption: foundVote && foundVote['voted_option']
-    };
+    }
   })
-  response.status(200).json(results);
-};
+  response.status(200).json(results)
+}
+
+const applyVote = async (request, response) => {
+  try {
+    console.log(request.body)
+    const { simReport, votedOption, userId } = request.body
+    const { articleAId, articleBId, voteId, userVoted } = simReport
+    var today = new Date()
+    var createdDate = new Date(today.getTime() - (today.getTimezoneOffset() * 60000))
+      .toISOString()
+      .split("T")[0]
+
+    const query = userVoted
+      ? `UPDATE public.vote SET voted_option = ${votedOption} WHERE id = ${voteId}`
+      : `
+        INSERT INTO public.vote (article_a_id, article_b_id, voted_option, user_id, created_date) 
+        VALUES (${articleAId}, ${articleBId}, ${votedOption}, ${userId}, '${createdDate}')
+      `
+    console.log('query', query)
+    await pool.query(query)
+    const updateSimReport = await getSimRecordById(simReport.id)
+    console.log('updateSimReport', updateSimReport)
+    response.status(200).json(updateSimReport)
+  } catch (error) {
+    throw error
+  }
+}
 
 export default {
-  getSimilarityRecords
-};
+  getSimilarityRecords,
+  applyVote
+}
