7fd0a8ab415759d85427d085c2aed655127bb31d
Tang Quoc Thai <victortang212@gmail.com>
2021-03-12 10:51:07 +0700

    Update metadate mapping

--- modules/ml/constants.py
+++ modules/ml/constants.py
@@ -2,5 +2,5 @@ META_MAPPING = {
     "domain": ["author_fullname", "newspaper"],
     "url": ["url", "href"],
     "publish_date": ["publish_date"],
-    "title": ["topic"],
+    "title": ["topic", "title"],
 }
