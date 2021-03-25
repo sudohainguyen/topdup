677e9a4fa71e861fa7be97cbdd6484aa5a14adde
Cong Cuong PHAM <ccuong.ph@gmail.com>
2021-03-25 16:52:50 +0000

    Update modules/web_app/frontend/src/components/about.js

--- modules/web_app/frontend/src/components/about.js
+++ modules/web_app/frontend/src/components/about.js
@@ -4,7 +4,7 @@ export default function About() {
   return (
       <>
         TopDup là một dự án mã nguồn mở nhằm nâng cao nhận thức về bản quyền nội dung số trong cộng đồng công nghệ Việt Nam.
-        Đây là dự án được đóng góp bởi rất nhiều thành viên trong [Forum Machine Learning cơ bản](https://www.facebook.com/groups/machinelearningcoban).
+        Đây là dự án được đóng góp bởi rất nhiều thành viên trong <a href="https://www.facebook.com/groups/machinelearningcoban" target="_blank">Forum Machine Learning cơ bản</a>.
       </>
   )
 }
